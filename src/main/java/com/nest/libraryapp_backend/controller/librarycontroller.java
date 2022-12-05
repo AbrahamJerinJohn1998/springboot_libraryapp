4b8c9fe04b49a4917f9103df962e012dc007e0ad
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-12-05 21:32:29 +0530

    postmapping

--- src/main/java/com/nest/libraryapp_backend/controller/librarycontroller.java
+++ src/main/java/com/nest/libraryapp_backend/controller/librarycontroller.java
@@ -2,41 +2,42 @@ package com.nest.libraryapp_backend.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class librarycontroller {
-    @GetMapping("/")
+    @PostMapping("/")
     public String homepage()
     {
         return "Welcome home page";
     }
-    @GetMapping("/bookEntry")
+    @PostMapping("/bookEntry")
     public String bookEntry()
     {
         return "Welcome book entry page";
     }
-    @GetMapping("/bookEdit")
+    @PostMapping("/bookEdit")
     public String bookEdit()
     {
         return "Welcome book edit page";
     }
-    @GetMapping("/bookSearch")
+    @PostMapping("/bookSearch")
     public String bookSearch()
     {
         return "Welcome book search page";
     }
-    @GetMapping("/bookDelete")
+    @PostMapping("/bookDelete")
     public String bookDelete()
     {
         return "Welcome book delete page";
     }
-    @GetMapping("/bookIssue")
+    @PostMapping("/bookIssue")
     public String bookIssue()
     {
         return "Welcome book issue page";
     }
-    @GetMapping("/viewBooks")
+    @PostMapping("/viewBooks")
     public String viewBooks()
     {
         return "Welcome view all books page";
